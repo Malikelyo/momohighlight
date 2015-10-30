89be297308a7de154d7421e2ef8755d00b5ae747
Malikelyo <hadesi2@163.com>
2015-10-30 19:56:24 +0800

    项目初始化

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by 邓涛 on 15/10/30.
+//  Copyright © 2015年 邓涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by 邓涛 on 15/10/30.
+//  Copyright © 2015年 邓涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
